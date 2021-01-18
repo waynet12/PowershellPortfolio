5a1ce0cee6a2b9a202bfb1a0dce432bc827649fc
waynet12 <waynewtay@gmail.com>
2021-01-17 16:26:00 -0800

    Added modules

---
+++ CheckTaskScheduler.ps1
@@ -0,0 +1,24 @@
+﻿#Get-TaskSchedulerResult Cmdlet- gets the status for tasks
+Function Get-TaskSchedulerResult{
+[CmdletBinding()]
+param (
+[string]$TaskName
+)
+BEGIN{
+
+#define credentials
+$userName = '' #Put username here
+$password = Get-Content '' #Put filepath for secure string password
+[Byte[]] $key = (1..16)
+
+#convert password to secure string
+[SecureString]$securePassword = $password | ConvertTo-SecureString -Key $key 
+
+#create credential object
+[PSCredential]$credObject = New-Object System.Management.Automation.PSCredential -ArgumentList $userName, $securePassword
+}
+PROCESS{
+#Checks the 
+Invoke-Command -ScriptBlock {Get-ScheduledTask -TaskName (Get-Content "\\Server01-dell\F_DRIVE\Bioseal Intranet Zone\IT Maintenance\Powershell\ServerMaintenance\CheckScheduledTasks\EpicorTaskNames.txt") | Get-ScheduledTaskInfo | Select-Object -Property TaskName,LastRunTime,LastTaskResult} -ComputerName "" #input computername that contains tasks
+}
+}
---
+++ GetServerBackup.ps1
@@ -0,0 +1,56 @@
+﻿#GetServerBackup Cmdlet- Checks server backup and outputs backup status.
+Function GetServerBackup {
+[CmdletBinding()]
+param
+(
+        [String]$ServerName
+) 
+
+#Create session for servers
+$rsession = New-PSSession -ComputerName $ServerName
+
+#Run backup status check command for servers
+$rstatus = Invoke-Command -Session $rsession -ScriptBlock {$status} 
+$runningstatus = Invoke-Command -Session $rsession -ScriptBlock {$runstatus} 
+disconnect-pssession $rsession 
+$hresult=$rstatus.errordescription 
+$outstatus="Success" 
+#condition to check when last backup happened and the 
+if ($rstatus.hresult -eq "0" -and !$hresult) 
+    { 
+    $Backup = "{0} {1} {2}" -f $ServerName, $outstatus, $rstatus.endtime 
+    } 
+elseif ($hresult.Contains("warnings")) 
+    { 
+    $Backup = "{0} Warning {1}" -f $ServerName, $rstatus.endtime 
+    } 
+else 
+    { 
+    $Backup = "{0} Failed {1}" -f $ServerName, $rstatus.endtime 
+    } 
+if ($runningstatus.CurrentOperation) 
+    {$Backup = $runningstatus.CurrentOperation} 
+Write-output $Backup 
+
+}
+
+#Get-ServerBackup Cmdlet passes GetServerBackup to get the backup status output
+function Get-ServerBackup {
+param ([string[]]$ServerName
+)
+BEGIN {
+$usedParameter =$false
+if($PSBoundParameters.ContainsKey('ServerName')) {
+$usedParameter = $true
+}
+}
+PROCESS {
+if ($usedParameter) {
+foreach ($computer in $ServerName) {
+GetServerBackup -ServerName $computer
+}
+} else {
+GetServerBackup -ServerName $_
+}
+}
+}
